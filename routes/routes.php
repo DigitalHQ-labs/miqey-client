83bf82352d16ea745552756553148d4da3c93883
Robin Rosiers <5263188+RosiersRobin@users.noreply.github.com>
2024-06-10 16:30:37 +0200

    Remove config webhook

--- routes/routes.php
+++ routes/routes.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Libaro\MiQey\Handlers\WebhookHandler;
 use Libaro\MiQey\Http\Controllers\ValidationController;
 
-Route::post(config('miqey.webhook_endpoint', '/miqey/webhook'), WebhookHandler::class)->name('miqey.webhook');
+Route::post('/miqey/webhook', WebhookHandler::class)->name('miqey.webhook');
 Route::get('/miqey/validate', ValidationController::class)->middleware('web')->name('miqey.validate');
