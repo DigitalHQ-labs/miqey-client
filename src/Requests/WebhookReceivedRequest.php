c7202c4ed7bf690f4e89c246f1e98ab2eed38643
Robin Rosiers <5263188+RosiersRobin@users.noreply.github.com>
2024-06-10 18:20:02 +0200

    Update WebhookReceivedRequest.php

--- src/Requests/WebhookReceivedRequest.php
+++ src/Requests/WebhookReceivedRequest.php
@@ -9,7 +9,7 @@ class WebhookReceivedRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'phone' => 'required|regex:^\+[1-9]\d{1,14}$',
+            'phone' => 'required',
             'code' => 'required',
         ];
     }
